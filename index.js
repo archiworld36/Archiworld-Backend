0054e636e5bb81a158adfc9160e3953465c4eb14
harsh7231 <harshrattewal@gmail.com>
2026-07-05 23:25:22 +0530

    api created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 // Body parser
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
